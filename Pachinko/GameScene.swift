014d143e5cc555aa004844e63ad89f372ee83a11
keertirajmalik <keertirajmalik@gmail.com>
2022-01-30 21:20:52 +0530

    Naming the slots and ball node

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -38,9 +38,11 @@ class GameScene: SKScene {
         if isGood {
             slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+            slotBase.name = "good"
         } else {
             slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+            slotBase.name = "bad"
         }
         
         slotBase.position = position
@@ -73,6 +75,7 @@ class GameScene: SKScene {
             ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
             ball.physicsBody?.restitution = 0.4
             ball.position = location
+            ball.name = "ball"
             addChild(ball)
         }
     }
